18f5a5d8cf145127af0f08c1f668cca1d1f65e7c
Brayan Delgado <brayancancer22@gmail.com>
2024-10-31 02:16:29 -0500

    Update front interfaces

--- assets/js/user_perfil.js
+++ assets/js/user_perfil.js
@@ -4,3 +4,8 @@ const profileDropdown = document.getElementById('profileDropdown');
 profileButton.addEventListener('click', () => {
     profileDropdown.style.display = profileDropdown.style.display === 'block' ? 'none' : 'block';
 });
+
+//Abrir Nueva Pagina
+function openNewPage() {
+    window.location.href = 'edit_perfil_user.html';
+}
