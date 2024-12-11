66eda01175728caf0b5e1abb1f1270c5d924dfd5
Bryan Daniel <danieljihuallanca42@gmail.com>
2024-12-11 01:39:03 -0500

    Cambios en la interfaz

--- public/assets/js/user_perfil.js
+++ public/assets/js/user_perfil.js
@@ -1,6 +1,8 @@
 // Manejar el menú desplegable del perfil
 const profileButton = document.getElementById('profileButton');
 const profileDropdown = document.getElementById('profileDropdown');
+const tabs = document.querySelectorAll('.tab');
+const contents = document.querySelectorAll('.content');
 profileButton.addEventListener('click', () => {
     profileDropdown.style.display = profileDropdown.style.display === 'block' ? 'none' : 'block';
 });
@@ -9,3 +11,15 @@ profileButton.addEventListener('click', () => {
 function openNewPage() {
     window.location.href = 'edit_perfil_user.html';
 }
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        // Remove 'active' class from all tabs and contents
+        tabs.forEach(t => t.classList.remove('active'));
+        contents.forEach(c => c.classList.remove('active'));
+
+        // Add 'active' class to the clicked tab and corresponding content
+        tab.classList.add('active');
+        document.getElementById(tab.getAttribute('data-target')).classList.add('active');
+    });
+});
